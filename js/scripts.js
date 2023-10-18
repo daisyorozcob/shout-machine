77d957a643c76fec475a92c034fab0997fe2cdf7
daisyorozcob <daisyyy.ob@gmail.com>
2023-10-18 10:50:15 -0700

    add user input function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 window.onload = function() {
     const form = document.querySelector("form");
-    
-}
+    form.onsubmit = function (event) {
+        const userAnswer = document.querySelector("input#user-input").ariaValueMax;
+        document.querySelector("p#answer").innerText= userAnswer.toUpperCase();
+        event.preventDefault();
+    };
+
+};
